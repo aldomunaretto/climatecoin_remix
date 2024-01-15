b002badaab2a7af717c8aefdde0d5ee602d74948
aldomunaretto <aldomunaretto@gmail.com>
2024-01-15 08:26:13 +0100

    Cleaning and merging code

--- ClimateCoinProject.sol
+++ ClimateCoinProject.sol
@@ -9,9 +9,9 @@ contract ClimateCoin is ERC20{
     // Variables de Estado
     address public owner;
 
-    constructor(uint256 initialSupply) ERC20("ClimateCoin", "CC") {
+    //Contructor
+    constructor() ERC20("ClimateCoin", "CC") {
         owner = msg.sender;
-        _mint(owner, initialSupply*10**decimals());
     }
 
     // Modificadores
@@ -42,10 +42,7 @@ contract ClimateCoinNFT is ERC721 {
 
     // Variables de Estado
     uint256 public tokenId;
-    address private _owner;
-    uint256 private feePercentage = 1; // 1% por defecto
-    ClimateCoin public climateCoin;
-    uint256 private initialClimateCoins = 297565 * 10 ** climateCoin.decimals();
+    address private owner;
 
     // Datos adicionales para cada ClimateCoinNFT
     struct NFTData {
@@ -56,68 +53,65 @@ contract ClimateCoinNFT is ERC721 {
 
     mapping(uint256 => NFTData) private _nftData;
 
-
-
     // Modificadores
     modifier onlyOwner() {
-        require(msg.sender == _owner, "Esta funcion solo puede ser llamada por el creador del contrato");
+        require(msg.sender == owner, "Esta funcion solo puede ser llamada por el creador del contrato");
         _;
     }
 
     constructor() ERC721("ClimateCoinNFT", "CCNFT") {
-        _owner = msg.sender;
+        owner = msg.sender;
     }
 
     // Función para Mintear ClimateCoinNFT
-    function mintNFT(address developerAddress, string memory projectName, string memory projectURL, uint256 credits) onlyOwner external {
-        _mint(developerAddress, tokenId);
+    function mint(address developerAddress, string memory projectName, string memory projectURL, uint256 credits) onlyOwner external returns (uint256) {
+        uint256 thisToken = tokenId;
+        _mint(developerAddress, thisToken);
+        _nftData[thisToken] = NFTData(projectName, projectURL, credits);
+        setApprovalForAll(developerAddress, true);
         tokenId++;
-        _nftData[tokenId] = NFTData(projectName, projectURL, credits);
-        emit NFTMinted(tokenId, developerAddress, projectName, projectURL, credits);
+        return thisToken;
+
     }
 
     // Función para obtener los datos de un ClimateCoinNFT
     function getNFTData(uint256 _tokenId) public view returns (string memory, string memory, uint256) {
         // Revisar que existe el NFT
-        require(_exists(_tokenId), "Error!");
         NFTData memory data = _nftData[_tokenId];
         return (data.projectName, data.projectURL, data.credits);
     }
-
-
-
-
 
 }
 
 contract ClimateCoinExchange {
 
     //Variables de estado
+    address private owner;
     ClimateCoin public climateCoin;
-    ClimateNFT public climateNFT;
+    ClimateCoinNFT public climateCoinNFT;
     uint256 public feePercentage = 1;
 
     // Eventos
     event NFTMinted(uint256 indexed tokenId, address indexed developerAddress, string projectName, string projectURL, uint256 credits);
-    event NFTExchanged(address indexed nftAddress, uint256 indexed tokenId, address indexed user);
+    event NFTExchanged(address indexed nftAddress, uint256 indexed tokenId, address indexed user, uint256 credits);
     event CCBurn(uint256 indexed tokenId, uint256 ccAmount);
 
     // Modificadores
     modifier onlyOwner() {
-        require(msg.sender == _owner, "Esta funcion solo puede ser llamada por el creador del contrato");
+        require(msg.sender == owner, "Esta funcion solo puede ser llamada por el creador del contrato");
         _;
     }
 
     constructor() {
+        owner = msg.sender;
         climateCoin = new ClimateCoin();
-        climateNFT = new ClimateNFT();
+        climateCoinNFT = new ClimateCoinNFT();
     }
 
+    // Función para Mintear ClimateCoinNFT
     function mintNFT(uint256 credits, string memory projectName, string memory projectURL, address developerAddress) public onlyOwner {
-        uint256 tokenId = climateNFT.totalSupply() + 1;
-        climateNFT.mint(developerAddress, tokenId, projectURL);
-        climateCoin.mint(developerAddress, credits);
-        emit NFTMinted(developerAddress, tokenId, credits);
+        uint256 _tokenId = climateCoinNFT.mint(developerAddress, projectName, projectURL, credits);
+        emit NFTMinted(_tokenId, developerAddress, projectName, projectURL, credits);
     }
 
     // Función para cambiar el porcentaje de la comisión
@@ -125,94 +119,32 @@ contract ClimateCoinExchange {
         feePercentage = newFeePercentage;
     }
 
-    function exchangeNFTForCC(address nftAddress, uint256 nftId) public {
-        require(climateNFT.ownerOf(nftId) == msg.sender, "Not the owner");
-        climateNFT.transferFrom(msg.sender, address(this), nftId);
-        uint256 credits = climateCoin.balanceOf(msg.sender);
-        uint256 fee = (credits * feePercentage) / 100;
-        uint256 finalAmount = credits - fee;
-        climateCoin.transfer(msg.sender, finalAmount);
-        climateCoin.transfer(owner(), fee);
-        emit NFTExchanged(msg.sender, nftId, finalAmount);
-    }
-
-
-    function setFeePercentage(uint256 newFeePercentage) external onlyOwner {
-        feePercentage = newFeePercentage;
-    }
-
     //Función de Intercambio de ClimateCoinNFT por ClimateCoins
-    function exchangeNFTForCC(address nftAddress, uint256 _tokenId) external {
+    function exchangeNFTForCC(address nftAddress, uint256 nftId) public {
         // Lógica para intercambiar ClimateCoinNFT por ClimateCoins, teniendo en cuenta la comisión
         // Transferir NFT al contrato y CC al usuario
-        climateCoin = ClimateCoin(initialClimateCoins);
-        emit NFTExchanged(nftAddress, _tokenId, msg.sender);
+        require(climateCoinNFT.ownerOf(nftId) == msg.sender, "No eres el propietario del climateCoinNFT");
+        (,,uint256 credits) = climateCoinNFT.getNFTData(nftId);
+        climateCoinNFT.transferFrom(msg.sender, address(this), nftId);
+        climateCoin.mint(msg.sender, credits * 10 ** climateCoin.decimals());
+        // uint256 credits = climateCoin.balanceOf(msg.sender);
+        uint256 fee = (credits * feePercentage) / 100;
+        uint256 finalAmount = credits - fee;
+        // climateCoin.transfer(msg.sender, finalAmount);
+        // climateCoin.transfer(owner, fee);
+        climateCoin.transferFrom(msg.sender, owner, fee);
+        emit NFTExchanged(nftAddress, nftId, msg.sender, finalAmount);
     }
 
     //Función de Quema de ClimateCoins y ClimateCoinNFT
     function burnCCAndNFT(uint256 ccAmount) public {
         // Lógica para seleccionar y destruir un ClimateCoinNFT y quemar los ClimateCoins asociados al mismo. ¿Como vinculo el ccAmount a un tokenId?
-        require(climateCoin.balanceOf(msg.sender) >= ccAmount, "Not enough CC");
-        climateCoin.burn(ccAmount);
-        uint256 tokenId = climateNFT.totalSupply();
-        climateNFT.burn(tokenId);
-        emit CCBurn(tokenId, ccAmount);
+        // require(climateCoin.balanceOf(msg.sender) >= ccAmount, "Not enough CC");
+        // climateCoin.burn(ccAmount);
+        // uint256 tokenId = climateNFT.totalSupply();
+        // climateNFT.burn(tokenId);
+        // emit CCBurn(tokenId, ccAmount);
     }
 
-}
-
-
-
-
-
-
-
-
-contract IntercambioSeguro {
-    address public seller;
-    uint256 public price; // Cantidad del ERC20 para poder comprar el NFT
-    address public erc20TokenAddress; // Dirección del token (por ej USDC)
-    address public erc721TokenAddress; // Dirección del contrato del NFT
-    uint public erc721TokenId; // Id del NFT dentro del contrato ERC721
-
-
-
-    constructor (uint _price, address _erc20TokenAddress, address _erc721TokenAddress, uint _erc721TokenId) {
-        seller = msg.sender;
-        price = _price;
-        erc20TokenAddress = _erc20TokenAddress;
-        erc721TokenAddress = _erc721TokenAddress;
-        erc721TokenId = _erc721TokenId;
-    }
-
-    function executeTrade() public {
-        IERC20 erc20Token = IERC20(erc20TokenAddress);
-        IERC721 erc721Token = IERC721(erc721TokenAddress);
-
-        bool erc20TransferSuccessful = erc20Token.transferFrom(msg.sender, seller, price);
-        require(erc20TransferSuccessful, "Transferencia del ERC20 no satisfactoria. Recuerda que debes de tener fondos suficientes y de hacer el allowance de minimo el precio a este contrato.");
-
-        erc721Token.transferFrom(seller, msg.sender, erc721TokenId);
-    }
-}
-
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract ClimateCoin is ERC20 {
-    constructor() ERC20("ClimateCoin", "CC") {}
-}
-
-contract ClimateNFT is ERC721 {
-    constructor() ERC721("ClimateNFT", "CNFT") {}
-
-    function mint(address to, uint256 tokenId, string memory tokenURI) public {
-        _mint(to, tokenId);
-        _setTokenURI(tokenId, tokenURI);
-    }
 }
 
